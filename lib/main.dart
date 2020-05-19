143f269f3fe9db85aeeaea269da3c2c485aea73d
Yao Josué <jyao2000@yahoo.fr>
2020-05-19 11:51:39 +0200

    Design

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,32 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Center(child: Text('Ask Me Anything')),
+          ),
+          body: ballWidget(),
+        ),
       ),
     );
+
+class ballWidget extends StatefulWidget {
+  @override
+  _ballWidgetState createState() => _ballWidgetState();
+}
+
+class _ballWidgetState extends State<ballWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Container(
+          child: Expanded(
+            child: Image.asset('images/ball1.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
